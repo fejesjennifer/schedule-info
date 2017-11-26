819d64a3c7f3589f638e468251d8368b99fe02ba
fejesjennifer <fejesjenni@gmail.com>
2017-11-26 20:40:10 +0100

    add bkkliveservice

--- src/controller/schedule.php
+++ src/controller/schedule.php
@@ -1,7 +1,11 @@
 <?php
+require_once ("../config.php");
+
+require_once ('../model/BKKLive.php');
 require_once('../model/DummyService.php');
 require_once('../model/TextFileService.php');
 require_once('../model/BKKService.php');
+
 
 // 1. bemenő adatok, validálni
 if(!isset($_GET['stop']) || !preg_match('/^[0-9A-Z]*$/',$_GET['stop'])) {
@@ -11,12 +15,17 @@ if(!isset($_GET['stop']) || !preg_match('/^[0-9A-Z]*$/',$_GET['stop'])) {
 }
 
 // 2. model-beli osztálynak valami függvényét
-$scheduleService = new TextFileService();
-$departures = $scheduleService->getDepartures($stop);
+//$scheduleService = new TextFileService();
+//$departures = $scheduleService->getDepartures($stop);
+$link=getbkkurl($stop);
+$service=new BKKLive($link);
+$departures=$service->getDepartures();
+
 
 //var_dump($departures);
 
 // 3. meghívunk egy view-t
 require('../view/schedule.php');
+
 ?>
 
